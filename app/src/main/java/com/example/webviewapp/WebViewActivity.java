7be534e0bce4b64a4e23f08fe6eee37316d90d7b
Diego Bruno <ogeid_hhh@hotmail.com>
2022-06-21 13:57:20 -0500

    Aplicacion terminada

--- app/src/main/java/com/example/webviewapp/WebViewActivity.java
+++ app/src/main/java/com/example/webviewapp/WebViewActivity.java
@@ -4,14 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class WebViewActivity extends AppCompatActivity {
+
+    WebView webView;
+    WebSettings webSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        webView = findViewById(R.id.wv_pagina_web);
+        webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://bruno-creatividad.web.app/");
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if((keyCode == KeyEvent.KEYCODE_BACK) &&  webView.canGoBack()){
+            webView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 
     @Override
